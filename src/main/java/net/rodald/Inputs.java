3fea263f88b3319ec91e3ea500d04bb318800dc7
Rodald <poltdavid@icloud.com>
2025-03-04 21:48:11 +0100

    added an exercise to the Inputs class;

--- src/main/java/net/rodald/Inputs.java
+++ src/main/java/net/rodald/Inputs.java
@@ -3,6 +3,9 @@ package net.rodald;
 import java.util.Scanner;
 
 public class Inputs {
+
+    /// Exercise: create a program that reads in 2 numbers from the user
+    ///  and then outputs the result of the operations {@code +}, {@code -}, {@code *}, {@code /}, and {@code %}
     public static void main(String[] args) {
         // Title: Getting User Input(Integer)
 
@@ -28,15 +31,14 @@ public class Inputs {
 
         // Title: Flaw when using Integers and Strings together
 
-        // Step 1: Create the Scanner
-        Scanner scanner = new Scanner(System.in);
-
-        // Step 2: Getting the User Input and storing it
-        System.out.println("Please enter a number: ");
-        int input = scanner.nextInt();
-        System.out.println("Please enter a string: ");
-        String line1 = scanner.nextLine();
-
-
+//        // Step 1: Create the Scanner
+//        Scanner scanner = new Scanner(System.in);
+//
+//        // Step 2: Getting the User Input and storing it
+//        System.out.println("Please enter a number: ");
+//        int input = scanner.nextInt();
+//        System.out.println("Please enter a string: ");
+//        // scanner.nextLine();
+//        String line1 = scanner.nextLine();
     }
 }
