bd5ded93622d0203a471663e18ab96f5e7d242dd
Rodald <poltdavid@icloud.com>
2025-03-13 07:51:55 +0100

    branching update;

---
+++ src/main/java/net/rodald/Branching.java
@@ -0,0 +1,50 @@
+package net.rodald;
+
+public class Branching {
+    public static void main(String[] args) {
+        // Title: booleans basics
+        boolean onGround = true;
+
+
+        // Title: boolean operators
+//        boolean inputA = true;
+//        boolean inputB = true;
+//
+//        // if both inputA and inputB are true (and gate)
+//        boolean output = inputA && inputB;
+//
+//        // if either inputA or inputB is true (or gate)
+//        boolean output = inputA && inputB
+//
+//        // inverts the current value (not gate)
+//        boolean output = !inputA;
+
+
+//        int numberA = 23;
+//        int numberB = 46;
+//
+//        boolean timesTwo = 2 * numberA == numberB;
+
+//        // combine multiple operators
+//        boolean isOp = false;
+//        boolean isDead = true;
+//        boolean onGround = true;
+//
+//        boolean playerValue = isDead && (onGround || isOp);
+
+        // Exercise: create a xor logic gate. There are 2 inputs (a, b).
+        //  The output should only be on if one of the inputs is set to true
+
+
+        // Title: if-statements
+
+//        boolean onGround = true;
+//        boolean canJump;
+//
+//        if (onGround) {
+//            canJump = true;
+//        } else {
+//            canJump = false;
+//        }
+    }
+}
